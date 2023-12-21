da5d58e6d1bd7364f5981303c0299cd0c11fc1f0
luthfinovra <lutvinovra@gmail.com>
2023-12-21 11:15:54 +0700

    Fix Styling and add Pagination

--- controllers/film.js
+++ controllers/film.js
@@ -1,5 +1,7 @@
 const Film = require('../models/film');
 
+const ITEMS_PER_PAGE = 35;
+
 module.exports.renderDetailFilm = async (req, res) => {
     data = await Film.find({id: req.params.id});
     data = data[0];
@@ -7,33 +9,89 @@ module.exports.renderDetailFilm = async (req, res) => {
 }
 
 module.exports.renderFilmPopuler = async (req, res) => {
-    films = await Film.find({}).sort({'rating': -1});
-    films = films.slice(0,100);
-    res.render('film/populer', {films});
-}
+    const page = parseInt(req.query.page) || 1; // Get the page from the query parameter, default to 1
+    const skip = (page - 1) * ITEMS_PER_PAGE; // Calculate the number of items to skip
 
+    try {
+        const totalFilms = await Film.countDocuments(); // Get the total number of films
+
+        // Fetch films for the current page using skip and limit
+        const films = await Film.find({}).sort({ 'rating': -1 }).skip(skip).limit(ITEMS_PER_PAGE);
+
+        res.render('film/populer', {
+            films,
+            currentPage: page,
+            hasNextPage: ITEMS_PER_PAGE * page < totalFilms,
+            hasPreviousPage: page > 1,
+            nextPage: page + 1,
+            previousPage: page - 1,
+            lastPage: Math.ceil(totalFilms / ITEMS_PER_PAGE)
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
 module.exports.renderFilmRekomendasi = async (req, res) => {
-    films = await Film.find({}).sort({'rating': -1});
-    films = films.slice(100,200);
-    res.render('film/rekomendasi', {films});
+    const page = parseInt(req.query.page) || 1; // Get the page from the query parameter, default to 1
+    const skip = (page - 1) * ITEMS_PER_PAGE; // Calculate the number of items to skip
+
+    try {
+        const totalFilms = await Film.countDocuments(); // Get the total number of films
+
+        // Fetch films for the current page using skip and limit
+        const films = await Film.find({}).sort({ 'rating': -1 }).skip(skip).limit(ITEMS_PER_PAGE);
+
+        res.render('film/rekomendasi', {
+            films,
+            currentPage: page,
+            hasNextPage: ITEMS_PER_PAGE * page < totalFilms,
+            hasPreviousPage: page > 1,
+            nextPage: page + 1,
+            previousPage: page - 1,
+            lastPage: Math.ceil(totalFilms / ITEMS_PER_PAGE)
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 }
 
 module.exports.renderFilmCari = async (req, res) => {
     let query = req.query.search;
-
+    
     if (!query) {
         query = 'Anda tidak mencari apapun';
         let result = ""
         return res.render('film/cari', {query, result});
     }
-
+    
     const isModalSearch = req.query.modal === 'true';
-
+    
     if(isModalSearch){
         const result = await Film.find({'judul': {$regex: `${query}`, $options: 'i'}}, ['judul', 'tahunRilis', 'rating', 'link_cover']).sort({'rating': -1}).limit(50);
         res.json(result);
     }else{
-        const result = await Film.find({'judul': {$regex: `${query}`, $options: 'i'}}).sort({'rating': -1}).limit(50);
-        res.render('film/cari', {query, result});
+        try {
+            const page = parseInt(req.query.page) || 1; // Get the page from the query parameter, default to 1
+            const skip = (page - 1) * ITEMS_PER_PAGE; // C
+            
+            // Fetch films for the current page using skip and limit
+            const totalFilms = await Film.find({'judul': {$regex: `${query}`, $options: 'i'}}).sort({'rating': -1}).countDocuments(); // Get the total number of films
+            const result = await Film.find({'judul': {$regex: `${query}`, $options: 'i'}}).sort({'rating': -1}).skip(skip).limit(ITEMS_PER_PAGE);
+            res.render('film/cari', {
+                query,
+                result,
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalFilms,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalFilms / ITEMS_PER_PAGE)
+            });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+        }
     }
 }
